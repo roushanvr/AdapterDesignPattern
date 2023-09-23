f3a6474bf7fab70cffe4f35b637c941a6ed28863
roushankumar12321@gmail.com <roushankumar12321@gmail.com>
2023-09-23 05:38:23 +0530

    Initial Commit

--- src/adapter/Phonepe.java
+++ src/adapter/Phonepe.java
@@ -2,8 +2,8 @@ package adapter;
 
 public class Phonepe {
     private BankApi bankAPI;
-    public Phonepe(BankApi api){
-       this.bankAPI=api;
+    public Phonepe(BankApi bankAPI){
+       this.bankAPI=bankAPI;
     }
     public void doSomething() throws InterruptedException{
        //
